8ef7935e3d41a30fc38435c3df922bff572fb7ca
一个小瘪三 <10948399+menon-qiqi@user.noreply.gitee.com>
2025-02-20 22:52:52 +0800

    feat-wip: 后端路由转换

--- apps/web-antd/src/router/index.ts
+++ apps/web-antd/src/router/index.ts
@@ -1,9 +1,10 @@
 import { Router } from '@xpress-core/router';
 
-import { testFrontendRoutes } from './helper';
+import { testBackendRoutes } from './helper';
 
 // const router = new Router(routesConfig);
-const router = new Router(testFrontendRoutes);
+// const router = new Router(testFrontendRoutes);
+const router = new Router(testBackendRoutes);
 
 export default router;
 
