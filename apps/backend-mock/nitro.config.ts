fcf9c44f50264ab9a333747869ab141ab353c24f
qyh <qyh@knownsec.com>
2025-03-31 14:13:45 +0800

    chore: 测试vercel部署nitro

--- apps/backend-mock/nitro.config.ts
+++ apps/backend-mock/nitro.config.ts
@@ -5,6 +5,17 @@ export default defineNitroConfig({
   devErrorHandler: errorHandler,
   errorHandler: '~/error',
   routeRules: {
+    '/': {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers':
+          'Accept, Authorization, Content-Length, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With',
+        'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Expose-Headers': '*',
+      },
+    },
     '/api/**': {
       cors: true,
       headers: {
@@ -16,5 +27,11 @@ export default defineNitroConfig({
         'Access-Control-Expose-Headers': '*',
       },
     },
+  },
+  // Vercel specific configuration
+  output: {
+    dir: '.output',
+    serverDir: '.output/server',
+    publicDir: '.output/public',
   },
 });
