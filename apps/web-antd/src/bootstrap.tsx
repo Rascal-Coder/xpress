5b4fc76ee3a06dbcf8dda1e493c9992594b994a6
qyh <qyh@knownsec.com>
2025-04-11 18:04:22 +0800

    feat: tanstack-query

--- apps/web-antd/src/bootstrap.tsx
+++ apps/web-antd/src/bootstrap.tsx
@@ -6,10 +6,14 @@ import {
 import { history, HistoryRouter } from '@xpress-core/router';
 
 import { ThemeProvider } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import { theme } from './constants/adapter';
+
+// 创建 QueryClient 实例
+const queryClient = new QueryClient();
 
 export const bootstrap = ({
   initialPreferences,
@@ -26,15 +30,17 @@ export const bootstrap = ({
   const root = createRoot(rootElement);
 
   root.render(
-    <HistoryRouter history={history}>
-      <PreferencesProvider
-        initialPreferences={initialPreferences}
-        preferenceManager={preferenceManager}
-      >
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </PreferencesProvider>
-    </HistoryRouter>,
+    <QueryClientProvider client={queryClient}>
+      <HistoryRouter history={history}>
+        <PreferencesProvider
+          initialPreferences={initialPreferences}
+          preferenceManager={preferenceManager}
+        >
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </PreferencesProvider>
+      </HistoryRouter>
+    </QueryClientProvider>,
   );
 };
